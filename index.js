6bfa7d2e5fced2c841780b9393342c29c6f8ba28
James <james@jamespodles.com>
2021-02-01 14:46:03 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+ /**
+ *
+ * Vue-Video-Player
+ * Adapted from Videojs (https://github.com/videojs/video.js)
+ *
+ */
+
+window.videojs = require('video.js')
+require('video.js/dist/video-js.css')
+
+var playerComponent = require('./player.vue').default;
+var videoPlayer = {
+  videojs: videojs,
+  videoPlayer: playerComponent,
+  install: function(Vue) {
+    Vue.component('video-player', playerComponent)
+  }
+}
+
+module.exports = videoPlayer
